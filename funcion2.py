269d3a6086d3cf9d0e298a60464090f1166c292b
Inugomu <antotiare11@gmail.com>
2026-05-08 14:24:28 -0400

    Add files via upload

---
+++ funcion2.py
@@ -0,0 +1,6 @@
+y = float(input("Ingrese un valor para Y : "))
+while(y != 0):
+    x = y ** 3 +7
+    print(f"para y = {y} ---> f(x) = {x}")
+    y = float(input("Ingrese un valor para Y : "))
+    
